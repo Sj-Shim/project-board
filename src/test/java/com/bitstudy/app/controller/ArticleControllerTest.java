f21c3269881b73ba8c397b22b4a231f775e45da5
Sj-Shim <lastfolio227@gmail.com>
2023-01-04 16:56:47 +0900

    #20 - 게시글 관련 뷰 테스트 작성
    어떤 경로로 뷰를 열지 테스트 명세 작성

--- src/test/java/com/bitstudy/app/controller/ArticleControllerTest.java
+++ src/test/java/com/bitstudy/app/controller/ArticleControllerTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /** 이 테스트 코드를 작성하고 실행시 결과적으로 404 발생
  * : 아직 ArticleController에 작성된 내용 없고, DAO같은 클래스도 없기 때문
@@ -20,7 +24,48 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
+    @DisplayName("게시판 리스트 페이지")
     @Test
-    void articlePage() throws Exception{
+    void articlesPage() throws Exception{
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+    //뷰를 만들고있으니까 HTMl로 코드를 짤 것이므로 /articles로 받아온 데이터의 미디어타입이 html타입으로 되어있는지 확인
+    //contentType : exact match라서 미디어타입이 딱 text/html로 나오는 것만 허용
+    //contentTypeCompatibleWith : 호환되는 타입까지 허용해줌
+                .andExpect(view().name("articles/index"))
+                //가져온 뷰 파일 이름 체크 (articles폴더 안의 index 인지 확인중)
+                .andExpect(model().attributeExists("articles"));
+                //가져온 뷰에서 게시글들이 떠야하는데 그 말은 서버에서 데이터들을 가져왔다는 뜻이므로 모델 attribute에
+                // articles가 있어야 할 것이다. 이 여부를 확인하는 테스트. 맵에 articles라는 키가 있는지 검사
+    }
+
+    @DisplayName("게시글 상세 페이지")
+    @Test
+    void articlePage() throws Exception {
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
+    }
+
+    @DisplayName("게시글 검색 전용 페이지 - 정상호출")
+    @Test
+    void articlesSearch() throws Exception{
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search"));
+    }
+
+    @DisplayName("해시태그 검색 전용 페이지")
+    @Test
+    void hashtagSearch() throws Exception {
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search-hashtag"));
     }
 }
