0c45090484ac2ac3cd1b6fa94ab99f255cf6ba6c
Sj-Shim <lastfolio227@gmail.com>
2023-01-12 18:05:15 +0900

    #31 - 게시판 페이지 검색바 기능 구현 + 뷰 파트
    검색시 searchType, searchValue 페이징 부분과 리스트의 타이틀 부분 클릭해도 검색 유지되도록 함.
    SearchType.java (enum) 을 한글로 출력되도록 description 설정

--- src/main/java/com/bitstudy/app/controller/ArticleController.java
+++ src/main/java/com/bitstudy/app/controller/ArticleController.java
@@ -62,6 +62,8 @@ public class ArticleController {
 
         map.addAttribute("articles", articles);
         map.addAttribute("paginationBarNumbers", barNumbers);
+        map.addAttribute("searchTypes", searchType.values());
+        /*enum .values() : enum인 요소들을 배열로 넘겨준다.*/
 
         return "articles/index";
     }
--- src/main/java/com/bitstudy/app/domain/type/SearchType.java
+++ src/main/java/com/bitstudy/app/domain/type/SearchType.java
@@ -1,5 +1,12 @@
 package com.bitstudy.app.domain.type;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum SearchType {
-    TITLE, CONTENT, ID, NICKNAME, HASHTAG
+    TITLE("제목"), CONTENT("내용"), ID("아이디"), NICKNAME("닉네임"), HASHTAG("해시태그");
+
+    private final String description;
 }
